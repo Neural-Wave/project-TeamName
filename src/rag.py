29c44bb07d184f003d679caa92d56703f1e2fb57
Carla López <carla@climate.ai>
2024-10-26 19:58:32 +0000

    different search type implementations

--- src/rag.py
+++ src/rag.py
@@ -6,21 +6,21 @@ from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from lingua import Language, LanguageDetectorBuilder
 from langchain_core.vectorstores import VectorStore
-from copy import deepcopy
-
-PERSON = {
-  'EN' : 'You',
-  'FR' : 'Vous',
-  'IT' : 'Tu',
-  'DE' : 'Du'
-}
 
 import textwrap
+from copy import deepcopy
+import re
 
+PERSON = {
+  'EN': 'You',
+  'FR': 'Vous',
+  'IT': 'Tu',
+  'DE': 'Du'
+}
 
 class SwisscomRAGChat:
-    def __init__(self, vector_store: VectorStore, language='EN', k = 10):
-        self.search_type = "similarity" # "similarity" - "similarity-threshold" - "mmr"
+    def __init__(self, vector_store: VectorStore, language='EN', k=10):
+        self.search_type = "bm25"
         self.k = k
         self.temperature = 0.3
         self.language = language
@@ -48,7 +48,7 @@ class SwisscomRAGChat:
         
         # Initialize language model and prompt template
         self.main_llm = ChatOpenAI(model="gpt-4o", temperature=self.temperature)
-        
+
         # Define the prompt with system and human messages
         prompt = ChatPromptTemplate.from_messages(
             [
@@ -57,8 +57,7 @@ class SwisscomRAGChat:
             ]
         )
         
-        self.search_kwargs = {'k': self.k, 'filter':{'language': self.language}}
-        # Initialize the chain using the llm and prompt
+        self.search_kwargs = {'k': self.k, 'filter': {'language': self.language}}
         self.chain = create_stuff_documents_chain(self.main_llm, prompt)
 
     def detect_lang(self, question):
@@ -72,82 +71,100 @@ class SwisscomRAGChat:
 
     def get_system_prompt(self):
         return """
-        You are a Swisscom website chatbot, designed to provide clear, professional assistance to potential and 
-        existing customers.  Your responses should be limited to information provided in the context and avoid lists. 
-        Include relevant links only when specifically mentioned in the context, and try to continue the conversation by 
-        asking relevant follow-up questions. If unsure of an answer, ask if the user would like to be forwarded to 
-        Swisscom customer support and clarify that your role is to assist only with Swisscom-related inquiries. 
-        If the user requests customer support, provide contact information: 0800 555 155, available Monday to Saturday 
-        from 8:00 to 20:00. Subtly suggest other relevant Swisscom products or services when possible. Respond in the 
-        same language as the user and, for irrelevant inquiries, avoid sharing additional information.
+You are a Swisscom website chatbot, designed to provide clear, professional assistance to potential and 
+existing customers. Your primary goal is to offer thorough, helpful responses that address user needs 
+while maintaining an engaging, friendly tone. Provide answers that are complete and informative, typically 
+between 50-100 words, to offer a richer experience without overwhelming the user. Ensure responses are in natural, flowing sentences. 
 
-        {context}
-        """
+Always include relevant links or suggest related Swisscom products when appropriate to the topic, even if not 
+explicitly requested, to enhance the user’s experience. Ask follow-up questions to keep the conversation going, 
+helping users explore Swisscom's offerings and find exactly what they need. For any unclear questions, kindly 
+ask for clarification and, if necessary, offer to connect them to Swisscom customer support. Preferably append the URL at the very end on a new line.
 
+If the user requests customer support, provide the following contact information: 0800 555 155, available 
+Monday to Saturday from 8:00 to 20:00. Clearly explain your role is to assist with Swisscom-related questions 
+and recommend support options as appropriate. Respond in the same language as the user, maintaining relevance, 
+and avoid elaborating on unrelated topics.
+
+{context}
+"""
 
     def check_default_language(self, detected_language):
-        # Detect the language of the question
         if detected_language != self.language:
           if detected_language in ['EN', 'FR', 'IT', 'DE']:
             self.language = detected_language
-            print('Chaning default language to:', self.language)
-
+            print('Changing default language to:', self.language)
             self.search_kwargs['filter']['language'] = self.language
 
     def invoke(self, inputs: dict, lang=True):
         question = inputs.get("input")
         
         detected_language = self.detect_lang(question)
-
         if lang:
-          self.check_default_language(detected_language)
+            self.check_default_language(detected_language)
         
-        # Do a search on the vector store
-        retriever = self.vector_store.as_retriever(
-          search_type=self.search_type, 
-          search_kwargs=self.search_kwargs
-        )
+        # Retrieve documents based on the search type
+        documents = self.retrieve_documents(question)
         
-        # Get relevant documents
-        documents = retriever.invoke(question)
-           
-        # Pass the filtered documents to the question-answer chain
-        response = self.chain.invoke({"input": question, "context": documents})
+        result = self.chain.invoke({"input": question, "context": documents})
+
+        return result, documents
+
+    def retrieve_documents(self, question):
+        if self.search_type == "bm25":
+            # BM25-based retrieval
+            documents = self.retriever.get_relevant_documents(question)
+
         
-        return response, documents
-      
+        elif self.search_type == "similarity":
+            # Similarity-based retrieval
+            retriever = self.vector_store.as_retriever(search_type="similarity", search_kwargs=self.search_kwargs)
+            documents = retriever.invoke(question)
+        
+        elif self.search_type == "rff":
+            # Combine BM25 and similarity results with Reciprocal Rank Fusion
+            similarity_retriever = self.vector_store.as_retriever(search_type="similarity", search_kwargs=self.search_kwargs)
+            bm25_docs = self.retriever.get_relevant_documents(question)
+
+            similarity_docs = similarity_retriever.invoke(question)
+
+            # RFF: Combine BM25 and similarity results
+            documents = self.reciprocal_rank_fusion(bm25_docs, similarity_docs)
+        
+        else:
+            raise ValueError("Unsupported search type")
+        
+        return documents
+
+    def reciprocal_rank_fusion(self, bm25_docs, similarity_docs):
+        # RFF implementation: give higher rank to documents that appear in both lists
+        combined_results = {}
+        for rank, doc in enumerate(bm25_docs):
+            combined_results[doc] = combined_results.get(doc, 0) + 1 / (rank + 1)
+        for rank, doc in enumerate(similarity_docs):
+            combined_results[doc] = combined_results.get(doc, 0) + 1 / (rank + 1)
+        
+        # Sort by combined RFF score
+        sorted_documents = sorted(combined_results, key=combined_results.get, reverse=True)
+        return sorted_documents[:self.k]
 
     def restart_chat_history(self):
         self.chat_history = []
         self.summary = ''
-
-        print('Sam:', self.invoke({'input':f'Express that you are starting anew with the conversation in {self.language}', 'context':None}, lang=False)[0])
-
+        print('Sam:', self.invoke({'input': f'Express that you are starting anew with the conversation in {self.language}'}, lang=False)[0])
 
     def invoke_with_memory(self, inputs, lang=True):
-        # Define a function to get responses with memory
-        
         question = inputs.get("input")
-        # Inject the current summary as context
         context_with_summary = f"{self.summary}\n\n{inputs}"
         
-        # Detect the language of the question
         detected_language = self.detect_lang(question)
         if lang:
           self.check_default_language(detected_language)
-
-        # Do a search on the vector store
-        retriever = self.vector_store.as_retriever(
-          search_type=self.search_type, 
-          search_kwargs=self.search_kwargs
-        )
-
-        # Retrieve relevant documents and generate answer
-        documents = retriever.invoke(question)
-        
+        documents = self.retrieve_documents(question)
         response = self.chain.invoke({"input": question, "context": documents, "summary": context_with_summary})
-        
-        # Update memory summary after each interaction
+        url = re.findall(r'(https://.+.html)"', documents[0].page_content)[0]
+        response = f"{response} {url}"
+
         self.summary = self.update_summary(inputs, response)
         
         return response, documents
@@ -185,45 +202,29 @@ class SwisscomRAGChat:
 
 
     def update_summary(self, input_text, response):
-        # Append new user and assistant message to the history
         self.chat_history.append(f"User: {input_text}\nAssistant: {response}")
-
-        # Join the history and summarize
         history_text = "\n".join(self.chat_history)
         new_summary = self.summarization_chain.invoke({"chat_history": history_text})
-
         return new_summary
-
 
     def chat(self):
       instructions = "Ask your question (or type 'exit' to quit): "
-      # info_msg = "You can find out more in"
       if self.language != 'EN':
-        instructions = self.invoke({'input':f'Translate {instructions} to {self.language}', 'context':None}, lang=False)[0]
-        # info_msg = self.invoke({'input':f'Translate {info_msg} to {self.language}', 'context':None}, lang=False)[0]
-
-      print('Sam:', self.invoke({'input':f'Default interaction language will be {self.language}. \
-          Introduce yourself in this language. The default language may change if the user changes languages.', 'context':None}, lang=False)[0])
+        instructions = self.invoke({'input': f'Translate {instructions} to {self.language}'}, lang=False)[0]
 
       while True:
-        # Get question from the user
         question = input(instructions)
-
-        # Break the loop if the user wants to exit
         if question.lower() in ["exit", "sortie", "uscita"]:
             exit_msg = "Thanks for chatting today! Have a nice day."
             if self.language != 'EN':
-              exit_msg = self.invoke({'input':f"Translate {exit_msg} to {self.language}", 'context':None}, lang=False)[0]
-            print("Sam:",  exit_msg)
+                exit_msg = self.invoke({'input': f"Translate {exit_msg} to {self.language}"}, lang=False)[0]
+            print("Sam:", exit_msg)
             break
 
-        # Use the chain to process the question
         try:
             print(f"{PERSON[self.language]}:", question, "\n")
             result, documents = self.invoke_with_memory({"input": question})
             wrapped_result = textwrap.fill(result, width=100)
             print("Sam:", wrapped_result, "\n")
-            # if len(documents):
-            #   print(f"{info_msg} {documents[0].metadata['source']} \n")
         except Exception as e:
             print(f"Error: {e}")
